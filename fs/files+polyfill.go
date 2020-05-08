090bc2b435c3d777b32aa501b67ebeeb2d48c01f
liuguoqiang <liuguoqiang@xiaoheiban.cn>
2020-05-08 11:31:03 +0800

    fix:ReadFile

--- fs/files+polyfill.go
+++ fs/files+polyfill.go
@@ -6,3 +6,16 @@ import "os"
 
 func CloseOnExec(*os.File) {
 }
+
+func ReadFile(path string) ([]byte, error) {
+	fi, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer fi.Close()
+	fd, err := ioutil.ReadAll(fi)
+	if err != nil {
+		return nil, err
+	}
+	return fd, nil
+}
